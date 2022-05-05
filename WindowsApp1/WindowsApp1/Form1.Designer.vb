972ea6f26767c122bb63629905bbd71c3cb3e0a0
evandcrowd <104784840+evandcrowd@users.noreply.github.com>
2022-05-05 13:33:58 -0400

    Added unzipped source code

---
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ButtonStart = New System.Windows.Forms.Button()
+        Me.ButtonStop = New System.Windows.Forms.Button()
+        Me.recordingList = New System.Windows.Forms.CheckedListBox()
+        Me.timeleftLabel = New System.Windows.Forms.Label()
+        Me.timeleftNumber = New System.Windows.Forms.Label()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnAbout = New System.Windows.Forms.Button()
+        Me.btnVersion = New System.Windows.Forms.Button()
+        Me.timer = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ApplicationName = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnMini = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ButtonStart
+        '
+        Me.ButtonStart.BackColor = System.Drawing.Color.Transparent
+        Me.ButtonStart.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(55, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(179, Byte), Integer))
+        Me.ButtonStart.FlatAppearance.BorderSize = 0
+        Me.ButtonStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer))
+        Me.ButtonStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer))
+        Me.ButtonStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonStart.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonStart.ForeColor = System.Drawing.Color.FromArgb(CType(CType(187, Byte), Integer), CType(CType(134, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.ButtonStart.Location = New System.Drawing.Point(293, 63)
+        Me.ButtonStart.Name = "ButtonStart"
+        Me.ButtonStart.Size = New System.Drawing.Size(94, 74)
+        Me.ButtonStart.TabIndex = 0
+        Me.ButtonStart.Text = "🞂"
+        Me.ButtonStart.UseVisualStyleBackColor = False
+        '
+        'ButtonStop
+        '
+        Me.ButtonStop.BackColor = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer))
+        Me.ButtonStop.FlatAppearance.BorderSize = 0
+        Me.ButtonStop.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer))
+        Me.ButtonStop.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer))
+        Me.ButtonStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonStop.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonStop.ForeColor = System.Drawing.Color.FromArgb(CType(CType(187, Byte), Integer), CType(CType(134, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.ButtonStop.Location = New System.Drawing.Point(423, 66)
+        Me.ButtonStop.Name = "ButtonStop"
+        Me.ButtonStop.Size = New System.Drawing.Size(51, 71)
+        Me.ButtonStop.TabIndex = 1
+        Me.ButtonStop.Text = "■"
+        Me.ButtonStop.UseVisualStyleBackColor = False
+        '
+        'recordingList
+        '
+        Me.recordingList.BackColor = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer))
+        Me.recordingList.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.recordingList.ForeColor = System.Drawing.Color.White
+        Me.recordingList.FormattingEnabled = True
+        Me.recordingList.Location = New System.Drawing.Point(12, 36)
+        Me.recordingList.Name = "recordingList"
+        Me.recordingList.Size = New System.Drawing.Size(267, 120)
+        Me.recordingList.TabIndex = 2
+        '
+        'timeleftLabel
+        '
+        Me.timeleftLabel.AutoSize = True
+        Me.timeleftLabel.ForeColor = System.Drawing.SystemColors.Control
+        Me.timeleftLabel.Location = New System.Drawing.Point(290, 36)
+        Me.timeleftLabel.Name = "timeleftLabel"
+        Me.timeleftLabel.Size = New System.Drawing.Size(80, 13)
+        Me.timeleftLabel.TabIndex = 3
+        Me.timeleftLabel.Text = "Time recording:"
+        '
+        'timeleftNumber
+        '
+        Me.timeleftNumber.AutoSize = True
+        Me.timeleftNumber.ForeColor = System.Drawing.Color.CornflowerBlue
+        Me.timeleftNumber.Location = New System.Drawing.Point(387, 33)
+        Me.timeleftNumber.Name = "timeleftNumber"
+        Me.timeleftNumber.Size = New System.Drawing.Size(0, 13)
+        Me.timeleftNumber.TabIndex = 4
+        '
+        'btnRemove
+        '
+        Me.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRemove.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnRemove.Location = New System.Drawing.Point(12, 167)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(139, 23)
+        Me.btnRemove.TabIndex = 6
+        Me.btnRemove.Text = "Remove selected from list"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'btnAbout
+        '
+        Me.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAbout.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnAbout.Location = New System.Drawing.Point(157, 167)
+        Me.btnAbout.Name = "btnAbout"
+        Me.btnAbout.Size = New System.Drawing.Size(67, 23)
+        Me.btnAbout.TabIndex = 7
+        Me.btnAbout.Text = "About"
+        Me.btnAbout.UseVisualStyleBackColor = True
+        '
+        'btnVersion
+        '
+        Me.btnVersion.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnVersion.ForeColor = System.Drawing.Color.White
+        Me.btnVersion.Location = New System.Drawing.Point(230, 167)
+        Me.btnVersion.Name = "btnVersion"
+        Me.btnVersion.Size = New System.Drawing.Size(67, 23)
+        Me.btnVersion.TabIndex = 8
+        Me.btnVersion.Text = "Version"
+        Me.btnVersion.UseVisualStyleBackColor = True
+        '
+        'timer
+        '
+        Me.timer.Interval = 1000
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(187, Byte), Integer), CType(CType(134, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.ProgressBar1.Location = New System.Drawing.Point(303, 167)
+        Me.ProgressBar1.MarqueeAnimationSpeed = 1000
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(179, 23)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 9
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(23, 22)
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'ApplicationName
+        '
+        Me.ApplicationName.AutoSize = True
+        Me.ApplicationName.BackColor = System.Drawing.Color.Transparent
+        Me.ApplicationName.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ApplicationName.ForeColor = System.Drawing.Color.Crimson
+        Me.ApplicationName.Location = New System.Drawing.Point(37, 2)
+        Me.ApplicationName.Name = "ApplicationName"
+        Me.ApplicationName.Size = New System.Drawing.Size(220, 18)
+        Me.ApplicationName.TabIndex = 11
+        Me.ApplicationName.Text = "SabaMeeting Auto-Recorder"
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnExit.FlatAppearance.BorderSize = 0
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(434, 0)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(56, 22)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "X"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnMini
+        '
+        Me.btnMini.BackColor = System.Drawing.Color.AliceBlue
+        Me.btnMini.FlatAppearance.BorderSize = 0
+        Me.btnMini.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMini.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMini.Location = New System.Drawing.Point(376, 0)
+        Me.btnMini.Name = "btnMini"
+        Me.btnMini.Size = New System.Drawing.Size(56, 22)
+        Me.btnMini.TabIndex = 13
+        Me.btnMini.Text = "-"
+        Me.btnMini.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(18, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(489, 199)
+        Me.Controls.Add(Me.btnMini)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.ApplicationName)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.btnVersion)
+        Me.Controls.Add(Me.btnAbout)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.timeleftNumber)
+        Me.Controls.Add(Me.timeleftLabel)
+        Me.Controls.Add(Me.recordingList)
+        Me.Controls.Add(Me.ButtonStop)
+        Me.Controls.Add(Me.ButtonStart)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "SabaMeeting Auto Recorder"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonStart As Button
+    Friend WithEvents ButtonStop As Button
+    Friend WithEvents recordingList As CheckedListBox
+    Friend WithEvents timeleftLabel As Label
+    Friend WithEvents timeleftNumber As Label
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents btnAbout As Button
+    Friend WithEvents btnVersion As Button
+    Friend WithEvents timer As Timer
+    Public WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ApplicationName As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnMini As Button
+End Class
---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,352 @@
+﻿Imports System.Net
+Imports System
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Threading
+Public Class Form1
+    Public Shared time As String
+    Public Shared timerecording As Integer
+    Public Shared recordingDone As Boolean
+    Public Shared datePartofDateandTime
+    Public Shared timePartofDateandTime
+    Public Shared convertedTime As String
+    Public Shared SabaID
+    Public Shared saba As Process()
+    Public Shared obs As Process()
+    Public Shared ProcessID
+    Public Shared meetingName
+    Public Shared i
+    Public Shared m
+    Public Shared p
+    Public Shared f As String
+    Dim userprofile As String = Environment.UserName
+    Private Declare Sub mouse_event Lib "user32.dll" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As IntPtr)
+    Public Sub PerformMouseClick(ByVal LClick_RClick_DClick As String)
+        Const MOUSEEVENTF_LEFTDOWN As Integer = 2
+        Const MOUSEEVENTF_LEFTUP As Integer = 4
+        Const MOUSEEVENTF_RIGHTDOWN As Integer = 6
+        Const MOUSEEVENTF_RIGHTUP As Integer = 8
+        If LClick_RClick_DClick = "RClick" Then
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, IntPtr.Zero)
+        ElseIf LClick_RClick_DClick = "LClick" Then
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, IntPtr.Zero)
+        ElseIf LClick_RClick_DClick = "DClick" Then
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, IntPtr.Zero)
+        End If
+    End Sub
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Add recordings to list.
+        Dim strFileSize As String = ""
+        Dim di As New IO.DirectoryInfo("C:\Users\" & userprofile & "\Desktop\Recordings")
+        Dim aryFi As IO.FileInfo() = di.GetFiles("*.ccrz")
+        Dim fi As IO.FileInfo
+        For Each fi In aryFi
+            strFileSize = (Math.Round(fi.Length / 1024)).ToString()
+            Console.WriteLine("File Name: {0}", fi.Name)
+            Console.WriteLine("File Full Name: {0}", fi.FullName)
+            Console.WriteLine("File Size (KB): {0}", strFileSize)
+            Console.WriteLine("File Extension: {0}", fi.Extension)
+            recordingList.Items.Add(fi.Name)
+            Dim filename As String = fi.Name
+        Next
+        recordingList.SetItemChecked(0, False)
+    End Sub
+    Public Sub ButtonStart_Click(sender As Object, e As EventArgs) Handles ButtonStart.Click
+        If (Not System.IO.Directory.Exists("C:\Users\" & userprofile & "\Desktop\Recordings")) Then
+            System.IO.Directory.CreateDirectory("C:\Users\" & userprofile & "\Desktop\Recordings")
+            System.IO.Directory.CreateDirectory("C:\Users\" & userprofile & "\Desktop\Recordings\Extracted Recording\")
+            System.IO.Directory.CreateDirectory("C:\Users\" & userprofile & "\Videos\Recording Retentions\")
+
+        End If
+
+        Dim recordingDone As Boolean
+
+        Dim fileList As New List(Of String)
+        Dim strFileSize As String = ""
+        Dim di As New IO.DirectoryInfo("C:\Users\" & userprofile & "\Desktop\Recordings")
+        Dim aryFi As IO.FileInfo() = di.GetFiles("*.ccrz")
+        Dim fi As IO.FileInfo
+        For Each fi In aryFi
+            strFileSize = (Math.Round(fi.Length / 1024)).ToString()
+            Console.WriteLine("File Name: {0}", fi.Name)
+            fileList.Add(fi.Name)
+            Console.WriteLine("File Full Name: {0}", fi.FullName)
+            Console.WriteLine("File Size (KB): {0}", strFileSize)
+            Console.WriteLine("File Extension: {0}", fi.Extension)
+            Dim filename As String = fi.Name
+
+        Next
+        'We will run through each indice
+
+        If recordingList.GetItemChecked(0) = True And timer.Enabled = False Then
+
+            Dim FILE_NAME As String = "C:\Users\" & userprofile & "\Desktop\Recordings\" & recordingList.Items(0)
+            If System.IO.File.Exists(FILE_NAME) = True Then
+                Console.WriteLine("Opening " + FILE_NAME)
+                Process.Start(FILE_NAME)
+                Thread.Sleep(5000)
+            Else
+                MsgBox("File Does Not Exist")
+            End If
+
+            'Extract CCRZ to Folder'
+            Try
+                ZipFile.ExtractToDirectory("C:\Users\" & userprofile & "\Desktop\Recordings\" + recordingList.Items(0), "C:\Users\" & userprofile & "\Desktop\Recordings\Extracted Recording")
+
+                Console.WriteLine("Extracted files.")
+
+            Catch ex As Exception
+
+            End Try
+
+            Dim di2 As New IO.DirectoryInfo("C:\Users\" & userprofile & "\Desktop\Recordings\Extracted Recording\")
+            Dim aryFi2 As IO.FileInfo() = di2.GetFiles("*.ccrf")
+            Dim fi2 As IO.FileInfo
+            For Each fi2 In aryFi2
+                Console.WriteLine("File Name: {0}", fi2.Name)
+                Console.WriteLine("File Full Name: {0}", fi2.FullName)
+                Console.WriteLine("File Extension: {0}", fi2.Extension)
+            Next
+            'Extracted File List
+
+
+            Thread.Sleep(2000)
+            'Open CCRF to get timestamp'
+            Dim strOrig As String = fi2.Name
+            If strOrig.Contains("._") Then
+                Dim output As String = strOrig.Substring(strOrig.IndexOf("._"))
+                output = output.Replace("._", "").Trim()
+                output = output.Replace(".ccrf", "").Trim()
+
+                datePartofDateandTime = output.Split("_")(0)
+                timePartofDateandTime = output.Split("_")(1)
+
+
+            Else
+
+                Dim output As String = strOrig.Substring(strOrig.IndexOf("_"))
+
+                i = output.IndexOf("_")
+                p = output.Substring(i + 1, output.IndexOf("_", i + 1) - i - 1)
+
+                output = output.Replace("_" + p + "_", "").Trim()
+                output = output.Replace(".ccrf", "").Trim()
+
+                datePartofDateandTime = output.Split("_")(0)
+                timePartofDateandTime = output.Split("_")(1)
+
+            End If
+
+            i = 2
+            While i < datePartofDateandTime.Length
+                datePartofDateandTime = datePartofDateandTime.Insert(i, "-")
+                i += 3
+            End While
+
+            timePartofDateandTime = timePartofDateandTime.Replace(".", ":")
+
+            Dim d As DateTime = DateTime.ParseExact(timePartofDateandTime, "HH:mm:ss", Nothing)
+
+            Dim sHour As String = d.ToString("HH")
+            Dim sMinute As String = d.ToString("mm")
+            Dim bAddHour As Boolean = False
+            Dim oIFP As IFormatProvider
+            Select Case CInt(sMinute)
+                Case 0 To 7
+                    sMinute = "00"
+                Case 8 To 22
+                    sMinute = "15"
+                Case 23 To 37
+                    sMinute = "30"
+                Case 38 To 52
+                    sMinute = "45"
+                Case 53 To 59
+                    sMinute = "00"
+                    bAddHour = True
+            End Select
+            d = DateTime.ParseExact(sHour & ":" & sMinute, "HH:mm", oIFP)
+            If bAddHour Then d = d.AddHours(1)
+            convertedTime = d.ToString("hh:mm tt")
+
+
+            convertedTime = convertedTime.Replace(":", "-")
+            convertedTime = convertedTime.Replace(" ", "")
+
+
+            Dim text As String = File.ReadAllText("C:\Users\" & userprofile & "\Desktop\Recordings\Extracted Recording\" & fi2.Name)
+            Dim index As Integer = text.IndexOf("<DURATION>")
+            Dim f As String = text.Substring(index + 1, text.IndexOf("</DURATION>", index + 1) - index - 1)
+
+
+            Dim index2 As Integer = text.IndexOf("<EVENT_NAME>")
+            Dim f2 As String = text.Substring(index2 + 1, text.IndexOf("</EVENT_NAME>", index2 + 1) - index2 - 1)
+            meetingName = f2.Replace("EVENT_NAME>", "")
+
+
+            'Set Timestamp to readable time instead of seconds
+            time = f.Replace("DURATION>", "")
+
+            Dim hms = TimeSpan.FromSeconds(time)
+            Dim h = hms.Hours
+            Dim mins = hms.Minutes
+            Dim s = hms.Seconds
+
+            RemoveFiles()
+
+
+            'Get Process ID's
+
+
+            saba = Process.GetProcessesByName("centra")
+            If saba.Length > 0 Then
+                For m = 0 To saba.Length - 1
+                    SabaID = (saba(m).Id)
+                Next
+            End If
+
+
+
+            obs = Process.GetProcessesByName("obs64")
+            If obs.Length > 0 Then
+                For p = 0 To obs.Length - 1
+                    ProcessID = (obs(p).Id)
+                Next
+            End If
+
+
+
+            'Start Timer
+            'Activate SabaMeeting Recording
+            AppActivate(SabaID)
+
+            'Start Saba Recording
+            Thread.Sleep(5000)
+            AppActivate("Saba Rec")
+            SendKeys.Send("{ENTER}")
+            'Start OBS Recording
+            AppActivate(ProcessID)
+            SendKeys.Send("{PGUP}")
+
+            'When timer hits 0 then stop recording
+
+            timer.Enabled = True
+            timer.Start()
+
+            'Set recording as checked so it goes to the next one.
+
+        ElseIf recordingList.Items.Count = 0 Then
+            MsgBox("All recordings have finished!!!!!!")
+
+        End If
+
+
+
+    End Sub
+
+
+    Private Sub ButtonStop_Click(sender As Object, e As EventArgs) Handles ButtonStop.Click
+
+        'Delete Files in Extracted Recordings'
+        ' Loop over the subdirectories and remove them with their contents
+        For Each d In Directory.GetDirectories("C:\Users\" & userprofile & "\Desktop\Recordings\Extracted Recording")
+            Directory.Delete(d, True)
+        Next
+
+        ' Finish removing also the files in the root folder
+        For Each f In Directory.GetFiles("C:\Users\" & userprofile & "\Desktop\Recordings\Extracted Recording")
+            File.Delete(f)
+        Next
+    End Sub
+    Public Function RemoveFiles()
+        'Delete Files in Extracted Recordings'
+        ' Loop over the subdirectories and remove them with their contents
+        For Each d In Directory.GetDirectories("C:\Users\" & userprofile & "\Desktop\Recordings\Extracted Recording")
+            Directory.Delete(d, True)
+        Next
+
+        ' Finish removing also the files in the root folder
+        For Each f In Directory.GetFiles("C:\Users\" & userprofile & "\Desktop\Recordings\Extracted Recording")
+            File.Delete(f)
+        Next
+        Dim str As String = "Useless"
+        Console.WriteLine("Removed Files from folder.")
+        Return str
+    End Function
+
+    Public Function Recording()
+        If recordingDone = True Then
+            AppActivate("OBS")
+            Thread.Sleep(7000)
+            SendKeys.Send("{PGDN}")
+            SendKeys.SendWait("{PGDN}")
+            Thread.Sleep(5000)
+
+            Try
+                My.Computer.FileSystem.RenameFile("E:\Recording Retentions\recording.mp4", datePartofDateandTime & "_" & convertedTime & "-" & meetingName & ".mp4")
+                recordingList.Items.RemoveAt(0)
+                recordingList.SetItemChecked(0, True)
+                recordingDone = False
+                timeleftNumber.Text = 0
+                timerecording = 0
+
+                ButtonStart.PerformClick()
+            Catch
+                If Not System.IO.File.Exists("E:\Recording Retentions\recording.mp4") Then
+                    Console.WriteLine("File not found. Going onto the next.")
+                    ButtonStart.PerformClick()
+                End If
+
+            End Try
+
+        End If
+    End Function
+
+    Private Sub BtnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        recordingList.Items.RemoveAt(recordingList.SelectedIndex)
+    End Sub
+
+    Private Sub BtnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
+        MsgBox("Application created by Evan Crowder for personal use with a SabaMeeting migration project for East Carolina University.")
+    End Sub
+
+    Private Sub BtnVersion_Click(sender As Object, e As EventArgs) Handles btnVersion.Click
+        MsgBox("Version 2.1" & vbCrLf & "Update notes:" & vbCrLf & "     Upgraded UI, made it accessible for laptops to use.  Rename file scheme.")
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles timer.Tick
+        timerecording += 1
+        timeleftNumber.Text = timerecording
+
+        If timerecording = time Then
+            recordingDone = True
+            timer.Enabled = False
+            Recording()
+            ProgressBar1.Value = 0
+            timerecording = 0
+            timeleftNumber.Text = "0"
+        Else
+            timer.Enabled = True
+            recordingDone = False
+            ProgressBar1.Value = ((timerecording / time) * 100)
+        End If
+
+        If ProgressBar1.Value > 100 Then
+            ProgressBar1.Value = 0
+        End If
+
+
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub BtnMini_Click(sender As Object, e As EventArgs) Handles btnMini.Click
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+    End Sub
+End Class
